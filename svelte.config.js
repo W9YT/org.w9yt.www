b8b14201db2dda844ca5624465120fabdb5994fa
HadleySo <71105018+HadleySo@users.noreply.github.com>
2026-02-04 19:50:30 -0600

    Adding favicon and sv build config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,16 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+	})},
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
 };
