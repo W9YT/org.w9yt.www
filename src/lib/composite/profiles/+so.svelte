23257a2d54e6d158dbd0864926ab83278711d401
HadleySo <71105018+HadleySo@users.noreply.github.com>
2026-03-19 22:06:53 -0500

    WACG fixes profile, so GPG

--- src/lib/composite/profiles/+so.svelte
+++ src/lib/composite/profiles/+so.svelte
@@ -16,4 +16,5 @@
 Madison, WI 53706"
     urlDepartment="https://pages.cs.wisc.edu/~hs/"
     urlPersonal="https://hadleyso.com/?utm_source=www.w9yt.org&ref=www.w9yt.org"
+    gpg="676F C5F3 03C1 C437 E0A0 E923 C52B 0689 6072 D856,https://keys.openpgp.org/vks/v1/by-fingerprint/676FC5F303C1C437E0A0E923C52B06896072D856"
 />
