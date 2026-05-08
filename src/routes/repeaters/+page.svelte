4ee4ddcbf6f39e0f9ef16954b7526b3952d05b85
Skylar <skylarculek@gmail.com>
2026-05-07 21:21:06 -0500

    Added 2m Repeater Splat Map (#19)
    * Added 2m Repeater Splat Map
    
    Splat added to news ticker, and the repeater information page.
    
    * Update src/lib/menus/lander-content.json
    
    Co-authored-by: Hadley So <71105018+HadleySo@users.noreply.github.com>
    
    * Update src/lib/menus/lander-content.json
    
    Co-authored-by: Hadley So <71105018+HadleySo@users.noreply.github.com>
    
    * Update src/routes/repeaters/+page.svelte
    
    Co-authored-by: Hadley So <71105018+HadleySo@users.noreply.github.com>
    
    * Update src/routes/news-events/w9yt-splat-map/+page.svx
    
    Co-authored-by: Hadley So <71105018+HadleySo@users.noreply.github.com>
    
    * Update src/routes/news-events/w9yt-splat-map/+page.svx
    
    Co-authored-by: Hadley So <71105018+HadleySo@users.noreply.github.com>
    
    * Update src/routes/repeaters/+page.svelte
    
    Co-authored-by: Hadley So <71105018+HadleySo@users.noreply.github.com>
    
    * Update src/routes/repeaters/+page.svelte
    
    Co-authored-by: Hadley So <71105018+HadleySo@users.noreply.github.com>
    
    * Update src/routes/news-events/w9yt-splat-map/+page.svx
    
    Co-authored-by: Hadley So <71105018+HadleySo@users.noreply.github.com>
    
    * Update src/routes/news-events/w9yt-splat-map/+page.svx
    
    Co-authored-by: Hadley So <71105018+HadleySo@users.noreply.github.com>
    
    * Revisions per HadleySo
    
    Added .pngs, rolled back .json changes.
    
    * Fixing image format for splat
    
    * Rollback 0046e891af3085e6cdc9d0920c8b758e1bd0ce3b
    
    ---------
    
    Co-authored-by: Hadley So <71105018+HadleySo@users.noreply.github.com>

--- src/routes/repeaters/+page.svelte
+++ src/routes/repeaters/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import splatMap from '$lib/img/splat2m.png'
 </script>
 
 <svelte:head>
@@ -13,19 +14,45 @@
 
 <div class="mx-auto max-w-3xl pt-10 text-lg px-5">
 
-	<h2 class="text-4xl/9 md:text-4xl/15 pb-10 font-semibold">
-		W9YT Madison Area Repeaters
-	</h2>
-	
-	<p class="py-4">
-		146.685 MHz with a PL of 123.0Hz 
-		<br>
-		The location of this repeater is atop Van Hise hall, the tallest building in Madison.
-	</p>
-	<p class="py-4">
-		443.600 MHz with a PL of 123.0Hz.  
-		<br>
-		This repeater also has Echolink capbility in addition to being a DMR node on the Brandmeister network and is also YSF capable.
-	</p>
+  <h2 class="text-4xl/9 md:text-4xl/15 pb-10 font-semibold">
+    W9YT Madison Area Repeaters
+  </h2>
+
+  <p class="py-4">
+    146.685 MHz with a PL of 123.0Hz
+    <br>
+    The location of this repeater is atop Van Hise hall, the tallest building in Madison.
+  </p>
+
+  <p class="py-4">
+    443.600 MHz with a PL of 123.0Hz.
+    <br>
+    This repeater also has Echolink capability in addition to being a DMR node on the Brandmeister network and is also YSF capable.
+  </p>
+
+  <h3 class="text-2xl pt-8 pb-4 font-semibold">
+    W9YT 2m Repeater Splat Map
+  </h3>
+
+  <p class="py-4">
+    <a
+      href="/pdfs/w9yt2msplat.pdf"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="underline"
+    >
+      Open PDF in new tab
+    </a>
+  </p>
+
+    <img src="{splatMap}" alt="W9YT 2M Repeater Splat Map" class="rounded-xl md:m-3 mb-3">
+
+    <embed
+    type="application/pdf"
+    src="/pdfs/w9yt2msplat.pdf"
+    width="100%"
+    height="900"
+    class="border rounded-xl border-1 border-gray-400 dark:border-gray-800"
+    title="W9YT Splat Map PDF"/>
 
 </div>
