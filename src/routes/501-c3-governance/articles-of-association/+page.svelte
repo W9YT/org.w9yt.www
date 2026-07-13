a43a78c31cda09d7450e7780a1195baef167e728
HadleySo <71105018+HadleySo@users.noreply.github.com>
2026-07-13 17:40:20 -0500

    Add AoA and 501c3 info

---
+++ src/routes/501-c3-governance/articles-of-association/+page.svelte
@@ -0,0 +1,140 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+	<meta property="og:title" content="Leadership | Badger Amateur Radio Society" />
+	<title>Articles of Association of Badger Amateur Radio Society | Badger Amateur Radio Society</title>
+	<meta
+		name="description"
+		content="The Badger Amateur Radio Society (BARS) is the amateur radio club at the University of Wisconsin-Madison"
+	/>
+	
+</svelte:head>
+
+<div class="mx-auto max-w-3xl px-5 pt-10 text-lg">
+	<h2 class="pb-10 text-4xl/9 md:text-4xl/15 font-semibold">Articles of Association</h2>
+
+	<p class="py-2 text-base">
+		The undersigned, a majority of whom are citizens of the United States, desiring to form a Non-Profit Corporation under the Non-Profit Corporation Law of WISCONSIN, do hereby certify:
+	</p>
+	<h3 class="pt-4 mt-2 text-lg">
+		Article 1 – Legal Name
+	</h3>
+	<p class="py-2 text-base">
+		The name of the Corporation shall be Badger Amateur Radio Society. The Corporation may also be referred to by the initialism BARS.
+	</p>
+	<h3 class="pt-4 mt-2 text-lg">
+		Article 2 – Principal Location
+	</h3>
+	<p class="py-2 text-base">
+		The place in this state where the principal office of the Corporation is to be located is the City of MADISON, DANE County.
+	</p>
+	<h3 class="pt-4 mt-2 text-lg">
+		Article 3 – Purpose of Association
+	</h3>
+	<p class="py-2 text-base">
+		Said Corporation is organized exclusively for charitable, educational, and scientific purposes, including, for such purposes, the making of distributions to organizations that qualify as exempt organizations under section 501(c)(3) of the Internal Revenue Code, or the corresponding section of any future federal tax code.
+	</p>
+	<p class="py-2 text-base">
+		The Corporation is organized for the purpose of educating, promoting, and supporting local amateur radio and amateur radio operators, also known as HAM radio, as defined by 47 C.F.R, Part 97 Amateur Radio Service.
+	</p>
+	<h3 class="pt-4 mt-2 text-lg">
+		Article 4 – Incorporators
+	</h3>
+	<p class="py-2 text-base">
+		The names and addresses of the persons who are the initial trustees of the Corporation are as follows:
+	</p>
+	<ul>
+		<li>
+			Skylar Paul Culek of *** at ***
+		</li>
+		<li>
+			Hadley So of *** at ***
+		</li>
+	</ul>
+	<p class="py-2 text-base">
+		The name and address for mail and communications shall be:
+	</p>
+	<p class="py-2 text-base">
+		Badger Amateur Radio Society<br>
+		1415 Engineering Dr<br>
+		Madison, WI 53706<br>
+		USA
+	</p>
+	<h3 class="pt-4 mt-2 text-lg">
+		Article 5 – Compensation
+	</h3>
+	<p class="py-2 text-base">
+		No part of the net earnings of the Corporation shall inure to the benefit of, or be distributable to its members, trustees, officers, or other private persons, except that the Corporation shall be authorized and empowered to pay reasonable compensation for services rendered and to make payments and distributions in furtherance of the purposes set forth in Article Third hereof. No substantial part of the activities of the Corporation shall be the carrying on of propaganda, or otherwise attempting to influence legislation, and the Corporation shall not participate in, or intervene in (including the publishing or distribution of statements) any political campaign on behalf of or in opposition to any candidate for public office. Notwithstanding any other provision of these articles, the Corporation shall not carry on any other activities not permitted to be carried on (a) by a corporation exempt from federal income tax under section 501(c)(3) of the Internal Revenue Code, or the corresponding section of any future federal tax code, or (b) by a corporation, contributions to which are deductible under section 170(c)(2) of the Internal Revenue Code, or the corresponding section of any future federal tax code.
+	</p>
+	<h3 class="pt-4 mt-2 text-lg">
+		Article 6 – Governance
+	</h3>
+	<p class="py-2 text-base">
+		An executive board shall be comprised of the following positions: President, Vice-president, and Treasurer.
+	</p>
+	<p class="py-2 text-base">
+		To be eligible to serve on the executive board, the individual must be 18 years of age and a member of the Corporation. Executive board members may be elected at any meeting by a majority vote of present Corporation members.
+	</p>
+	<p class="py-2 text-base">
+		All board members shall be elected to serve a one-year term; however, the term may be extended until a successor has been elected. Board members may serve terms in succession. The term of office shall be considered to begin and end on the first day of Fall Instruction as defined by University of Wisconsin-Madison Faculty Policies and Procedures UW-801 1.20.
+	</p>
+	<p class="py-2 text-base">
+		A board member may be removed by two-thirds vote of board members then in office, if the board member has shown a dereliction of duty.
+	</p>
+	<h3 class="pt-4 mt-2 text-lg">
+		Article 7 – Membership
+	</h3>
+	<p class="py-2 text-base">
+		Any persons possessing a sincere interest in amateur radio activity are welcome to join the Corporation. Membership of this Corporation does not include membership to the University of Wisconsin-Madison registered student organization Badger Amateur Radio Society. No dues shall be required for membership.
+	</p>
+	<p class="py-2 text-base">
+		The Corporation shall have no membership class and no members who have any right to vote or title or interest in or to the Corporation, its properties and franchises.
+	</p>
+	<p class="py-2 text-base">
+		At no time shall member information be shared with or sold to other organizations or groups without the member’s consent.
+	</p>
+	<p class="py-2 text-base">
+		A non-executive board member may be removed from the Corporation by a two-thirds vote of the executive board.
+	</p>
+	<h3 class="pt-4 mt-2 text-lg">
+		Article 7 – Distribution Upon Dissolution
+	</h3>
+	<p class="py-2 text-base">
+		Upon the dissolution of the Corporation, assets shall be distributed to the University of Wisconsin-Madison registered student organization Badger Amateur Radio Society, or for one or more exempt purposes within the meaning of section 501(c)(3) of the Internal Revenue Code, or the corresponding section of any future federal tax code, or shall be distributed to the federal government, or to a state or local government, for a public purpose.
+	</p>
+	<p class="py-2 text-base">
+		Priority and preference shall be given to the University of Wisconsin-Madison registered student organization Badger Amateur Radio Society, or the corresponding organization of any future University of Wisconsin-Madison registered student organization with the purpose as defined in the third article.
+	</p>
+	<p class="py-2 text-base">
+		Any such assets not so disposed of shall be disposed of by a Court of Competent Jurisdiction of the county in which the principal office of the Corporation is then located, exclusively for such purposes or to such organization or organizations, as said Court shall determine, which are organized and operated exclusively for such purposes.
+	</p>
+	<p class="py-2 text-base">
+		--
+	</p>
+	<p class="py-2 text-base">
+		In witness whereof, we have hereunto subscribed our names this 8th day of May 2025.
+	</p>
+	<code>
+		SKYLAR PAUL CULEK
+		<br>
+		5/11/2025 | 22:29:41 CDT
+		<br>
+		E2A7
+		<br>
+	</code>
+	<br>
+	<code>
+		HADLEY SO
+		<br>
+		5/12/2025 | 21:29:24 CDT
+		<br>
+		68A4
+		<br>
+	</code>
+	<br>
+	<code>
+		Docusign Envelope ID: C5A874AD-6541-4062-90EA-E1E3A2D29FEC
+	</code>
+</div>
